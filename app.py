5b45ac7e0ee8789cb7d7084ae331d57d1e194773
kglibrarian <karen.gutzman@northwestern.edu>
2019-06-26 19:22:43 -0500

    adding header links

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from sqlalchemy import create_engine, func
 from flask import (
     Flask,
     render_template,
+    url_for,
     jsonify,
     request,
     redirect)
@@ -60,20 +61,23 @@ def home():
 
 @app.route("/illinois")
 def mina():
-    return render_template("illinois.html")
+    return render_template("Illinois.html")
 
 @app.route("/kentucky")
 def Kentucky():
-    return render_template("kentucky.html")
+    return render_template("Kentucky.html")
 
 @app.route("/texas")
 def Texas():
-    return render_template("texas.html")
+    return render_template("Texas.html")
 
 @app.route("/brickey")
 def Brickey():
     return render_template("brickey.html")
 
+@app.route("/about")
+def About():
+    return render_template("about.html")
 
 @app.route("/api/v1.0/consumptionsector")
 
